422052611ce6e75158bb124a1654b3f3e3f0ca63
mariah622 <mjustine622@gmail.com>
2021-12-08 18:16:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(name, event){
+    const thankYou = []
+    for( let i = 0; i < name.length; i++) {
+        thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou
+}
+
+
+
+function countDown() {
+    let count = 10
+    while (count >= 0 ) {
+        console.log(count--)
+    }
+
+}
