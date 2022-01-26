504d8aa74aa10a147c9f514c2dde41837273f70a
mariah622 <mjustine622@gmail.com>
2022-01-26 13:55:45 -0500

    commit tree dbb5b8de17112b2818eec32e8f7bce05dcca61e2

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 
+// function writeCards(name, event){
+//     const thankYou = []
+//     for( let i = 0; i < name.length; i++) {
+//         thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+//     }
+//     return thankYou
+// }
+
+
+
+// function countDown() {
+//     let count = 10
+//     while (count >= 0 ) {
+//         console.log(count--)
+//     }
+
+// }
+
 function writeCards(name, event){
-    const thankYou = []
-    for( let i = 0; i < name.length; i++) {
-        thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    const messages = []
+    for(let i = 0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+
     }
-    return thankYou
+    return messages
+    
+
 }
 
-
-
-function countDown() {
-    let count = 10
-    while (count >= 0 ) {
-        console.log(count--)
+function countDown(){
+    for (let count = 10; count >= 0; count--){
+        console.log(count)
     }
+    
 
 }
